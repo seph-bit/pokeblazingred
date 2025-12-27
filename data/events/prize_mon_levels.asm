31920716648b887fdff44cad014834e3f56c8ceb
seph-bit <seraphim.beatle@gmail.com>
2025-12-26 20:41:46 -0600

    prize_mon_levels.asm

--- data/events/prize_mon_levels.asm
+++ data/events/prize_mon_levels.asm
@@ -1,19 +1,9 @@
 PrizeMonLevelDictionary:
 IF DEF(_RED)
 	db ABRA,      9
-	db CLEFAIRY,  8
-	db NIDORINA, 17
+	db PIKACHU,  12
+	db STARYU, 19
 
-	db DRATINI,  18
-	db SCYTHER,  25
+	db CLEFABLE,  23
+	db DRAGONAIR,  32
 	db PORYGON,  26
-ENDC
-IF DEF(_BLUE)
-	db ABRA,      6
-	db CLEFAIRY, 12
-	db NIDORINO, 17
-
-	db PINSIR,   20
-	db DRATINI,  24
-	db PORYGON,  18
-ENDC
